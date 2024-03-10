035f41c4845113135c1b86ba8729d0485ced3f8b
CanOguzz <mustafacanoguz@gmail.com>
2024-03-10 17:17:14 +0300

    changecound funtion problem solved secondsoundgroup array property name change key to keyTrigger

--- src/Components/DrumMachine/DrumMachine.jsx
+++ src/Components/DrumMachine/DrumMachine.jsx
@@ -60,60 +60,61 @@ const firstSoundsGroup = [
 
 
 const secondSoundsGroup = [ 
+        {
+      keyCode: 81,
+      keyTrigger: 'Q',
+      id: 'Chord-1',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+    },
     {
-    keyCode: 81,
-    key: 'Q',
-    id: 'Chord-1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
-  },
-  {
-    keyCode: 87,
-    key: 'W',
-    id: 'Chord-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
-  },
-  {
-    keyCode: 69,
-    key: 'E',
-    id: 'Chord-3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
-  },
-  {
-    keyCode: 65,
-    key: 'A',
-    id: 'Shaker',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
-  },
-  {
-    keyCode: 83,
-    key: 'S',
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
-  },
-  {
-    keyCode: 68,
-    key: 'D',
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
-  },
-  {
-    keyCode: 90,
-    key: 'Z',
-    id: 'Punchy-Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
-  },
-  {
-    keyCode: 88,
-    key: 'X',
-    id: 'Side-Stick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
-  },
-  {
-    keyCode: 67,
-    key: 'C',
-    id: 'Snare',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
-  }];
+      keyCode: 87,
+      keyTrigger: 'W',
+      id: 'Chord-2',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+    },
+    {
+      keyCode: 69,
+      keyTrigger: 'E',
+      id: 'Chord-3',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+    },
+    {
+      keyCode: 65,
+      keyTrigger: 'A',
+      id: 'Shaker',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+    },
+    {
+      keyCode: 83,
+      keyTrigger: 'S',
+      id: 'Open-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+    },
+    {
+      keyCode: 68,
+      keyTrigger: 'D',
+      id: 'Closed-HH',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+    },
+    {
+      keyCode: 90,
+      keyTrigger: 'Z',
+      id: 'Punchy-Kick',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+    },
+    {
+      keyCode: 88,
+      keyTrigger: 'X',
+      id: 'Side-Stick',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+    },
+    {
+      keyCode: 67,
+      keyTrigger: 'C',
+      id: 'Snare',
+      url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+    }
+];
 
 const soundsName = {
   heaterKit: "Heater Kit",
@@ -134,7 +135,10 @@ const KeyboardKey = ({ play, sound: { keyTrigger, url } }) => {
 
   React.useEffect(() => {
     document.addEventListener("keydown", handleKeydown);
-  }, [  ]);
+    return () => {
+        document.removeEventListener("keydown", handleKeydown);
+      };
+  }, []);
 
   return (
     <button className="drum-pad" onClick={() => play(keyTrigger)}>
@@ -152,14 +156,15 @@ const Keyboard = ({ play, sounds }) => {
 const DrumControle = ({ changeSoundGroup }) => {
   return (
     <div className="controle">
+        <h2 id="display" >asd</h2>
       <button onClick={changeSoundGroup}>Change Sound Group</button>
     </div>
   );
 };
 
 const DrumMachine = () => {
-  const [soundsType, setSoundType] = React.useState("heaterKit");
-  const [sounds, setSounds] = React.useState(soundsGroup[soundsType]);
+  const [soundType, setSoundType] = React.useState("heaterKit");
+  const [sounds, setSounds] = React.useState(soundsGroup[soundType]);
 
   
 
@@ -169,8 +174,9 @@ const DrumMachine = () => {
     audio.play().catch((error) => console.error("playback error: ", error));
   };
 
+  
   const changeSoundGroup = () => {
-    if(soundsType === "heaterKit"){
+    if(soundType === "heaterKit"){
         console.log('smoothPianoKit sounds:', soundsGroup.smoothPianoKit);
       setSoundType("smoothPianoKit");
       setSounds(soundsGroup.smoothPianoKit);
@@ -180,11 +186,13 @@ const DrumMachine = () => {
       setSounds(soundsGroup.heaterKit);
     }
   };
-
+  
+ 
   return (
     <div id="drum-machine">
       <Keyboard play={play} sounds={sounds} />
       <DrumControle changeSoundGroup={changeSoundGroup} />
+      
     </div>
   );
 };
